e433a9dcd89f4d64b01aedcf31ea58f513d9e00f
Tomokazu Kizawa <tomokazu.kizawa@outlook.com>
2026-06-06 20:22:10 +0900

    OpenFileDialog などの COM コンポーネントが動作するよう Program.cs に [STAThread] 属性を追加

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Windows.Forms;
 using PasswordManagerApp;
 
-// To customize application configuration such as set high DPI settings or default font,
-// see https://aka.ms/applicationconfiguration.
-ApplicationConfiguration.Initialize();
-Application.Run(new Form1());
+namespace PasswordManagerApp;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
